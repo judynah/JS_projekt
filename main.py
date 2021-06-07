25b7e404f5109a91a1dc4aa90bebb5e8c416db91
judynah <74621908+judynah@users.noreply.github.com>
2021-06-08 00:27:55 +0200

    v2

--- main.py
+++ main.py
@@ -5,61 +5,14 @@ from beverage import *
 from vendingMachine import *
 from machineInterface import *
 
-# Press the green button in the gutter to run the script.
-
 
 if __name__ == '__main__':
-    # m1 = Coin(1, 'PLN')
-    # m3 = Coin(1, 'PLN')
-    # m2 = Coin(2, 'PLN')
-    #
-    # p1 = MoneyKeeper()
-    #
-    # b1 = Beverage('Cola', 3.5)
-    # bk1 = BeverageKeeper()
-    # bk1.addItem(b1)
 
     vm = VendingMachine()
     vm.createbeverageList(30, 50)
-    vm.fillWithMoney(30)
-    #
-    # print(vm.currentBeverageList[0].itemNumber)
-    # print(vm.currentBeverageList[1].itemNumber)
-    # print(vm.currentBeverageList[-2].itemNumber)
-    # print(vm.currentBeverageList[-1].itemNumber)
-    # print(vm.currentBeverageList[0].beverageList[0].name)
-    # print(vm.currentBeverageList[1].beverageList[0].name)
-    # print(vm.currentBeverageList[-2].beverageList[0].name)
-    # print(vm.currentBeverageList[-1].beverageList[0].name)
-    #
-    # vm.removeBeverage(30)
-    # print(vm.currentBeverageList[0].beverageList)
-    # vm.removeBeverage(30)
-    # print(vm.currentBeverageList[0].beverageList)
-    # vm.removeBeverage(30)
-    # print(vm.currentBeverageList[0].beverageList)
-    # vm.removeBeverage(30)
-    # print(vm.currentBeverageList[0].beverageList)
-    # vm.removeBeverage(30)
-    # print(vm.currentBeverageList[0].beverageList)
-    # # vm.removeBeverage(30)
-    # # print(vm.currentBeverageList[0].beverageList)
-    # # price = round(vm.getPriceOfItem(36), 2)
-    # # print("Suma: ", vm.moneySum)
-    # #
-    # vm.sellBeverage(30)
-
-    # vm.sellBeverage(36)
-    # vm.sellBeverage(36)
-    # vm.sellBeverage(36)
-    # vm.sellBeverage(36)
-    # vm.sellBeverage(36)
-
-    # print(vm.moneySum)
-
-
+    vm.fillWithMoney(100)
     vmi = MachineInterface(vm)
-    # vmi.createWindow()
+
 
 
 
